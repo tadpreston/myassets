0241c0046ba16ad79a9044990180b90c9ac0aca4
Tad Preston <tadpreston@gmail.com>
2019-08-08 17:12:33 -0500

    Added authorization to the pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :require_login
+
+  private
+
+  def current_user
+    @current_user || User.find(session[:current_user_id]) if session[:current_user_id]
+  end
+
+  def require_login
+    redirect_to sessions_new_url, alert: "Not Authorized!" unless current_user
+  end
 end
