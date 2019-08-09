b6bd6883d65c8273a9f80f1d2e86695303e72860
Tad Preston <tadpreston@gmail.com>
2019-08-08 19:39:54 -0500

    updated the routes to reflect what should be done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users, only: [:edit, :update, :destroy]
+
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   root 'pages#index'
 end
