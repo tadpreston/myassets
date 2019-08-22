0760aa7ad2de29b826875d7d67d5ad56ef7347b3
Tad Preston <tadpreston@gmail.com>
2019-08-22 06:00:01 -0500

    make sure the user enters the space name

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,5 @@
 class Space < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe Space, type: :model do
   it do
     should have_db_column(:user_id).of_type(:integer)
-    should have_db_column(:name).of_type(:text)
+    should have_db_column(:name).of_type(:string)
     should have_db_column(:description).of_type(:text)
   end
 
   it { should belong_to(:user) }
+
+  it { should validate_presence_of(:name) }
 end
